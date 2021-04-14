e22f0b70cc1d92cd21e055ffc31c907c84d4bdec
Mary <marybsa135@gmail.com>
2021-04-14 13:43:45 -0300

    first commit

---
+++ bubble Sort/MainForm.Designer.cs
@@ -0,0 +1,77 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: DELL
+ * Date: 14/04/2021
+ * Time: 11:10
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace bubble_Sort
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.ListBox listBox1;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.BackColor = System.Drawing.Color.AliceBlue;
+			this.label1.Location = new System.Drawing.Point(59, 11);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(100, 23);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Bubble Sort";
+			// 
+			// listBox1
+			// 
+			this.listBox1.BackColor = System.Drawing.Color.PaleTurquoise;
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.Location = new System.Drawing.Point(59, 37);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(146, 212);
+			this.listBox1.TabIndex = 1;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.MidnightBlue;
+			this.ClientSize = new System.Drawing.Size(284, 261);
+			this.Controls.Add(this.listBox1);
+			this.Controls.Add(this.label1);
+			this.Name = "MainForm";
+			this.Text = "bubble Sort";
+			this.ResumeLayout(false);
+
+		}
+	}
+}
---
+++ bubble Sort/MainForm.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: DELL
+ * Date: 14/04/2021
+ * Time: 11:10
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace bubble_Sort
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			
+			InitializeComponent();
+			//nomes = {“julio”, “amelia”, “henrique”, “silvia”, “camila”, “rui”, “cesar”, “benedito”, “alonso”, “claudia”, “maria”, “lucas”, “renato”, “heloisa”, “vilma”, “wanessa”, “ricardo”, “daniela”, “mauro”, “antonio”}
+			
+			string[] nomes = {"julio","amelia", "henrique", "silva", "camila", "rui", "cesar", "benedito", "alonso", "claudia", "maria", "lucas", "renato", "heloisa", "vilma", "wanessa", "ricardo", "daniela", "mauro", "antonio"};
+		for (int j=0; j<19; j++) 
+			for (int i=0; i<19; i++){
+			string aux =""; 
+	  		if ( nomes[i].CompareTo(nomes[i+1]) >= 0 )
+	  		{
+	  			aux = nomes[i];
+	  			nomes[i] = nomes[i+1];
+	  			nomes[i+1] = aux;
+	  		}
+		}
+			
+		listBox1.Items.Clear(); 
+ 		for (int i=0; i<20; i++)
+		 listBox1.Items.Add(nomes[i]);
+		}
+	}
+}
